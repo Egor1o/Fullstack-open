66d9170e114c98db06f3f0a71df9ff06ed7cccce
marlions <egor.kovaelnko@aalto.fi>
2023-10-29 12:35:52 +0200

    part2 puhelinluettelo 2.8 version1

--- part2/puhelinluettelo/src/App.jsx
+++ part2/puhelinluettelo/src/App.jsx
@@ -1,33 +1,49 @@
 import { useState } from 'react'
 
-const Person = ({ name }) => {
+const Person = ({ name, number }) => {
 	return (
 		<>
-			<p>{name}</p>
+			<p>
+				{name} {number}
+			</p>
 		</>
 	)
 }
 
 const Info = ({ persons }) => {
-	console.log(persons)
+	//console.log(persons)
 	return persons.map((human) => {
-		return <Person key={human.name} name={human.name} />
+		return (
+			<Person key={human.name} name={human.name} number={human.number} />
+		)
 	})
 }
 
 const App = () => {
-	const [persons, setPersons] = useState([{ name: 'Arto Hellas' }])
-	const [newName, setNewName] = useState('')
+	const [persons, setPersons] = useState([
+		{ name: 'Arto Hellas', number: '+358146146146' },
+	])
+	const [newInfo, setNewInfo] = useState({ name: '', number: '' })
 
 	const handleAddClick = (event) => {
 		event.preventDefault()
-		const copy = [...persons]
-		copy.push({ name: newName })
-		setPersons(copy)
+		//console.log(newInfo)
+		//console.log(persons.map((elem) => elem.name).includes(newInfo.name))
+		if (persons.map((elem) => elem.name).includes(newInfo.name)) {
+			alert(`${newInfo} is already added to phonebook`)
+		} else {
+			const copy = [...persons]
+			copy.push({ name: newInfo.name, number: newInfo.number })
+			setPersons(copy)
+		}
 	}
 
-	const handleInputChange = (event) => {
-		setNewName(event.target.value)
+	const handleNameChange = (event) => {
+		setNewInfo({ ...newInfo, name: event.target.value })
+	}
+
+	const handleNumberChange = (event) => {
+		setNewInfo({ ...newInfo, number: event.target.value })
 	}
 
 	return (
@@ -35,7 +51,15 @@ const App = () => {
 			<h2>Phonebook</h2>
 			<form onSubmit={handleAddClick}>
 				<div>
-					name: <input value={newName} onChange={handleInputChange} />
+					name:
+					<input value={newInfo.name} onChange={handleNameChange} />
+				</div>
+				<div>
+					number:
+					<input
+						value={newInfo.number}
+						onChange={handleNumberChange}
+					/>
 				</div>
 				<div>
 					<button type='submit'>add</button>
