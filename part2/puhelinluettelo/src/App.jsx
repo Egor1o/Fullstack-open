d41da40a91223e2cd08945e2e41d262dc1eb9098
marlions <egor.kovaelnko@aalto.fi>
2023-11-03 22:37:04 +0200

    part2 2.12 version1 syncronization is added

--- part2/puhelinluettelo/src/App.jsx
+++ part2/puhelinluettelo/src/App.jsx
@@ -1,5 +1,5 @@
-import { useState, useEffect } from 'react'
 import axios from 'axios'
+import { useEffect, useState } from 'react'
 
 const Person = ({ name, number }) => {
 	return (
@@ -19,27 +19,38 @@ const Info = ({ persons }) => {
 	})
 }
 
-const NumbersDisplay = ({persons}) =>{
-	return <><h2>Numbers</h2>
+const NumbersDisplay = ({ persons }) => {
+	return (
+		<>
+			<h2>Numbers</h2>
 			<div>
 				<Info persons={persons} />
 			</div>
-			</>
+		</>
+	)
 }
 
-const Filter = ({search, handleSearchChnage}) =>{
-	return <>
-		<h2>Phonebook</h2>
+const Filter = ({ search, handleSearchChnage }) => {
+	return (
+		<>
+			<h2>Phonebook</h2>
 			<div>
-			filter shown with 
-		<input value={search} onChange={handleSearchChnage}/>
-		</div>
-	</>
+				filter shown with
+				<input value={search} onChange={handleSearchChnage} />
+			</div>
+		</>
+	)
 }
 
-const PersonForm = ({handleAddClick, handleNameChange, handleNumberChange, newInfo}) => {
-	return (<>
-	<h2>Add a new</h2>
+const PersonForm = ({
+	handleAddClick,
+	handleNameChange,
+	handleNumberChange,
+	newInfo,
+}) => {
+	return (
+		<>
+			<h2>Add a new</h2>
 			<form onSubmit={handleAddClick}>
 				<div>
 					name:
@@ -55,36 +66,42 @@ const PersonForm = ({handleAddClick, handleNameChange, handleNumberChange, newIn
 				<div>
 					<button type='submit'>add</button>
 				</div>
-			</form></>)
+			</form>
+		</>
+	)
 }
 
 const App = () => {
-	const [persons, setPersons] = useState([
-		{ name: 'Arto Hellas', number: '040-123456' },
-		{ name: 'Ada Lovelace', number: '39-44-5323523' },
-		{ name: 'Dan Abramov', number: '12-43-234345' },
-		{ name: 'Mary Poppendieck', number: '39-23-6423122' }
-	])
+	const [persons, setPersons] = useState([])
 	const [newInfo, setNewInfo] = useState({ name: '', number: '' })
 	const [serach, setSearch] = useState('')
 
 	//do not forget to turn server on!
-	useEffect(() =>{axios
-		.get('http://localhost:3002/persons')
-		.then(response => {
+	useEffect(() => {
+		axios.get('http://localhost:3002/persons').then((response) => {
 			console.log(response.data)
 			setPersons(response.data)
-		})},[])
+		})
+	}, [])
 
 	const handleAddClick = (event) => {
 		event.preventDefault()
 		if (persons.map((elem) => elem.name).includes(newInfo.name)) {
-			alert(`${newInfo} is already added to phonebook`)
+			alert(`${newInfo.name} is already added to phonebook`)
 		} else {
 			const copy = [...persons]
+			const newObj = {
+				name: newInfo.name,
+				number: newInfo.number,
+			}
 			copy.push({ name: newInfo.name, number: newInfo.number })
-			setPersons(copy)
-			setNewInfo({name: '', number: ''})
+			axios
+				.post(`http://localhost:3002/persons`, newObj)
+				.then((response) => {
+					console.log(response)
+					setPersons(copy)
+					setNewInfo({ name: '', number: '' })
+				})
 		}
 	}
 
@@ -96,23 +113,30 @@ const App = () => {
 		setNewInfo({ ...newInfo, number: event.target.value })
 	}
 
-
-	 const handleSearchChnage = (event) => {
+	const handleSearchChnage = (event) => {
 		setSearch(event.target.value)
 	}
 
 	const sortedSearch = (persons, search) => {
 		if (search !== '') {
-		 return persons.filter(person => person.name.toLowerCase().startsWith(search))
-		}
-		else return persons
+			return persons.filter((person) =>
+				person.name.toLowerCase().startsWith(search)
+			)
+		} else return persons
 	}
-	
+
 	return (
 		<div>
-			<Filter search={serach} handleSearchChnage={handleSearchChnage}/>
-			<PersonForm handleAddClick={handleAddClick} handleNameChange={handleNameChange} handleNumberChange={handleNumberChange} newInfo={newInfo} />
-			<NumbersDisplay persons={sortedSearch(persons,serach.toLowerCase())}/>
+			<Filter search={serach} handleSearchChnage={handleSearchChnage} />
+			<PersonForm
+				handleAddClick={handleAddClick}
+				handleNameChange={handleNameChange}
+				handleNumberChange={handleNumberChange}
+				newInfo={newInfo}
+			/>
+			<NumbersDisplay
+				persons={sortedSearch(persons, serach.toLowerCase())}
+			/>
 		</div>
 	)
 }
