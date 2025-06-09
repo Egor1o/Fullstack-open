b38c9e2765ad5e27e631587489fe25eb8c68c143
Egor1o <egorklo147@gmail.com>
2025-06-09 22:03:59 +0300

    Part 9 - 9.5

--- part9/index.ts
+++ part9/index.ts
@@ -1,11 +1,34 @@
-import express from 'express'
+import express, {Request} from 'express'
+import calculateBmi from "./bmiCalculator";
 const app = express();
+
+interface QueryParams {
+    weight?: string,
+    height?: string
+}
+
+app.get('/bmi', (req: Request<{}, {}, {}, QueryParams>, res) => {
+    const params = req.query
+    if(!params || !params.weight || !params.height || isNaN(Number(params.weight)) || isNaN(Number(params.weight))){
+        res.status(400).json({error: "malformatted parameters"})
+    }
+    try{
+        const bmiValue = calculateBmi(Number(params.height), Number(params.weight))
+        res.status(200).json({
+            weight: params.weight,
+            height: params.height,
+            bmi: bmiValue
+        })
+    } catch (_) {
+        res.status(400).json({error: "malformatted parameters"})
+    }
+});
 
 app.get('/hello', (_req, res) => {
     res.send('Hello Full Stack!');
 });
 
-const PORT = 3001;
+const PORT = 3003;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
