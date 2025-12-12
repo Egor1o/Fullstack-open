7239185c6c6637325cd7a7807e345c2c3771e55f
Egor1o <egorklo147@gmail.com>
2025-12-12 20:27:15 +0200

    part3-3.3

--- part3/index.js
+++ part3/index.js
@@ -32,6 +32,16 @@ app.get("/info", (req, res) => {
   );
 });
 
+app.get("/api/persons/:id", (req, res) => {
+  const id = req.params.id;
+  const person = phoneBook.find((p) => p.id === id);
+  if (person) {
+    res.json(person);
+  } else {
+    res.status(404).end();
+  }
+});
+
 app.get("/api/persons", (req, res) => {
   res.json(phoneBook);
 });
