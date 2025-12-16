faa055e6adaf4b1ab35ed37b1bf641218a2dfd1a
Egor1o <egorklo147@gmail.com>
2025-12-17 00:59:52 +0200

    part3-3.14-specific person with it's own id fetched from db

--- part3/index.js
+++ part3/index.js
@@ -50,12 +50,14 @@ app.get("/info", (req, res) => {
 
 app.get("/api/persons/:id", (req, res) => {
   const id = req.params.id;
-  const person = phoneBook.find((p) => p.id === id);
-  if (person) {
-    res.json(person);
-  } else {
-    res.status(404).end();
-  }
+
+  PhoneBookInstance.findById(id).then((result) => {
+    if (result) {
+      res.json(result);
+    } else {
+      res.status(404).end();
+    }
+  });
 });
 
 app.get("/api/persons", (req, res) => {
