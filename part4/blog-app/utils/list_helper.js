d4f65979b181ae755d858278ace241f2024a5a9f
Egor1o <egorklo147@gmail.com>
2025-12-21 22:10:00 +0200

    part4-4.4 Tests.

--- part4/blog-app/utils/list_helper.js
+++ part4/blog-app/utils/list_helper.js
@@ -2,6 +2,14 @@ const dummy = (blogs) => {
   return 1
 }
 
+//blogs is a list of blogs defined in models/blog.js
+const totalLikes = (blogs) => {
+  return blogs.reduce((acc, blogs) => {
+    return acc + blogs.likes
+  }, 0)
+}
+
 module.exports = {
   dummy,
+  totalLikes
 }
