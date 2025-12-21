b96f9ad64d375d880f04761e5a78a551a438a2c6
Egor1o <egorklo147@gmail.com>
2025-12-21 21:20:35 +0200

    part4-4.2 Adapted project to the structure.

---
+++ part4/blog-app/app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const blogsRouter = require("./controllers/blogs");
+const logger = require("./utils/logger");
+const middleware = require("./utils/middleware");
+const config = require("./utils/config");
+
+const app = express();
+
+mongoose
+  .connect(config.MONGODB_URI, { family: 4 })
+  .then(() => {
+    logger.info("Connected to MongoDB");
+  })
+  .catch((error) => {
+    logger.error("error connection to MongoDB:", error.message);
+  });
+
+app.use(express.json());
+app.use(middleware.requestLogger);
+app.use("/api/blogs", blogsRouter);
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
+
+module.exports = app;
