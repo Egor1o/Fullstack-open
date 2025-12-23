051a0e3fd4e8579efe53f710d7cdfbb27eef476c
Egor1o <egorklo147@gmail.com>
2025-12-23 20:04:54 +0200

    part4-4.9 Added test ensuring id exists and mapped in the response for each blog.

--- part4/blog-app/tests/blogs_api.test.js
+++ part4/blog-app/tests/blogs_api.test.js
@@ -1,6 +1,6 @@
 const supertest = require('supertest')
 const app = require('../app')
-const { test, describe, after, beforeEach } = require('node:test')
+const { test, after, beforeEach } = require('node:test')
 const assert = require('node:assert')
 const mongoose = require('mongoose')
 const helper = require('./test_helper')
@@ -13,11 +13,22 @@ beforeEach(async () => {
   await Blog.insertMany(helper.initialBlogs)
 })
 
-test('blogs are returned as json.', async () => {
+test('blogs are returned as json', async () => {
   const response = await api.get('/api/blogs')
   assert.strictEqual(response.body.length, helper.initialBlogs.length)
 })
 
+test('each blog has a mapped id property', async () => {
+  const response = await api.get('/api/blogs')
+  response.body.forEach((blog) => {
+    const id = Object.keys(blog).includes('id') ? 'id' : null
+    const _id = Object.keys(blog).includes('_id') ? '_id' : null
+    assert.strictEqual(id, 'id')
+    assert.strictEqual(_id, null)
+  })
+})
+
 after(async () => {
+  await Blog.deleteMany({})
   await mongoose.connection.close()
 })
