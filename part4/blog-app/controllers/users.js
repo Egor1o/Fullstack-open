b28a7c6bc648d942c39d51e170b547a3e3990a8f
Egor1o <egorklo147@gmail.com>
2025-12-28 18:47:58 +0200

    part4-4.17 added expiration time of 1 hour.

--- part4/blog-app/controllers/users.js
+++ part4/blog-app/controllers/users.js
@@ -42,7 +42,7 @@ userRouter.post('/login', async (request, response) => {
     id: user._id,
   }
 
-  const token = jwt.sign(userForToken, process.env.SECRET)
+  const token = jwt.sign(userForToken, process.env.SECRET, { expiresIn: 60*60 })
   return response.status(200).send({ token, username: user.username, name: user.name })
 })
 
