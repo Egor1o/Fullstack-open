cd58bb1fd9915bedc72e6db80db6f4c3b0fe4b3d
Egor1o <egorklo147@gmail.com>
2025-12-28 11:40:17 +0200

    part4-4.15 added user router and user mongoose schema.

---
+++ part4/blog-app/controllers/users.js
@@ -0,0 +1,20 @@
+const userRouter = require('express').Router()
+const User = require('../models/user')
+const bcrypt = require('bcrypt')
+
+const SALT_ROUNDS = 10
+
+userRouter.get('/', async (request, response) => {
+  const users = await User.find({})
+  return response.json(users)
+})
+
+userRouter.post('/', async (request, response) => {
+  const { username, name, password } = request.body
+  const passwordHash = await bcrypt.hash(password, SALT_ROUNDS)
+  const user = new User({ username, name, passwordHash })
+  const savedUser = await user.save()
+  response.status(201).json(savedUser)
+})
+
+module.exports = userRouter
