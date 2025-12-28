38a8023a5ba2f44b893379e9934815712c092c2b
Egor1o <egorklo147@gmail.com>
2025-12-28 18:45:55 +0200

    part4-4.17 login endpoint.

--- part4/blog-app/controllers/users.js
+++ part4/blog-app/controllers/users.js
@@ -1,6 +1,7 @@
 const userRouter = require('express').Router()
 const User = require('../models/users')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
 const SALT_ROUNDS = 10
 
@@ -21,6 +22,28 @@ userRouter.post('/', async (request, response) => {
   const user = new User({ username, name, passwordHash })
   const savedUser = await user.save()
   response.status(201).json(savedUser)
+})
+
+userRouter.post('/login', async (request, response) => {
+  const { username, password } = request.body
+
+  if(!username || !password) {
+    return response.status(400).json({ error: 'Username or password missing' })
+  }
+
+  const user = await User.findOne({ username })
+  const isValidPassword = await bcrypt.compare(password, user.passwordHash)
+  if(!user || !isValidPassword) {
+    return response.status(401).json({ error: 'Invalid username or password' })
+  }
+
+  const userForToken = {
+    username: user.username,
+    id: user._id,
+  }
+
+  const token = jwt.sign(userForToken, process.env.SECRET)
+  return response.status(200).send({ token, username: user.username, name: user.name })
 })
 
 module.exports = userRouter
