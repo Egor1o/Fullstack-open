0e8b2d0c7220b20610b89b83980a6f9558571a51
Egor1o <egorklo147@gmail.com>
2026-01-06 23:58:22 +0200

    part5-5.2 Permanent user storage.

--- part5/src/App.jsx
+++ part5/src/App.jsx
@@ -17,6 +17,7 @@ const App = () => {
 
     try {
       const user = await loginService.login({ username, password });
+      window.localStorage.setItem("loggedBlogappUser", JSON.stringify(user));
       blogService.setToken(user.token);
       setUser(user);
       setUsername("");
@@ -30,7 +31,20 @@ const App = () => {
   };
 
   useEffect(() => {
-    blogService.getAll().then((blogs) => setBlogs(blogs));
+    const loggedUserJSON = window.localStorage.getItem("loggedBlogappUser");
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON);
+      setUser(user);
+      blogService.setToken(user.token);
+    }
+  }, []);
+
+  useEffect(() => {
+    const getAllBlogs = async () => {
+      const allBlogs = await blogService.getAll();
+      setBlogs(allBlogs);
+    };
+    getAllBlogs();
   }, []);
 
   return (
