d8cb87ce89b2ca51840f6bb0486b4e665a9c1108
Egor1o <egorklo147@gmail.com>
2026-01-07 00:53:04 +0200

    part5-5.4 Blogs are showed only when logged in.

--- part5/src/App.jsx
+++ part5/src/App.jsx
@@ -66,11 +66,15 @@ const App = () => {
         </div>
       )}
 
-      <h2>blogs</h2>
+      {user && (
+        <>
+          <h2>blogs</h2>
 
-      {blogs.map((blog) => (
-        <Blog key={blog.id} blog={blog} />
-      ))}
+          {blogs.map((blog) => (
+            <Blog key={blog.id} blog={blog} />
+          ))}
+        </>
+      )}
     </div>
   );
 };
