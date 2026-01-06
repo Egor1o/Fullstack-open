5ea5863e99c555afd8c527a74eecc6968e17cb4b
Egor1o <egorklo147@gmail.com>
2026-01-06 23:45:13 +0200

    part5-5.1 Initial implementation with blog addition form. The form does not work.

---
+++ part5/src/App.jsx
@@ -0,0 +1,63 @@
+import { useState, useEffect } from "react";
+import Blog from "./components/Blog.jsx";
+import blogService from "./services/blogs.js";
+import loginService from "./services/login.js";
+import { LoginForm } from "./components/LoginForm.jsx";
+import { BlogForm } from "./components/BlogForm.jsx";
+
+const App = () => {
+  const [blogs, setBlogs] = useState([]);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [user, setUser] = useState(null);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const handleLogin = async (event) => {
+    event.preventDefault();
+
+    try {
+      const user = await loginService.login({ username, password });
+      blogService.setToken(user.token);
+      setUser(user);
+      setUsername("");
+      setPassword("");
+    } catch {
+      setErrorMessage("wrong credentials");
+      setTimeout(() => {
+        setErrorMessage(null);
+      }, 5000);
+    }
+  };
+
+  useEffect(() => {
+    blogService.getAll().then((blogs) => setBlogs(blogs));
+  }, []);
+
+  return (
+    <div>
+      {!user && (
+        <LoginForm
+          handleLogin={handleLogin}
+          password={password}
+          setPassword={setPassword}
+          setUsername={setUsername}
+          username={username}
+        />
+      )}
+
+      {user && (
+        <div>
+          <p>{user.name} logged in</p>
+          <BlogForm />
+        </div>
+      )}
+
+      <h2>blogs</h2>
+      {blogs.map((blog) => (
+        <Blog key={blog.id} blog={blog} />
+      ))}
+    </div>
+  );
+};
+
+export default App;
---
+++ part5/src/components/BlogForm.jsx
@@ -0,0 +1,6 @@
+export const BlogForm = ({addBlog, newBlog, handleBlogChange}) => (
+    <form onSubmit={addBlog}>
+        <input value={newBlog} onChange={handleBlogChange} />
+        <button type="submit">save</button>
+    </form>
+)
